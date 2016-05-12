99ec2f7ce0cda7ba2dca9756b711db84fb7e4d18
Victor255 <tagbar95@gmail.com>
2016-05-12 12:22:02 -0600

    the first version of the project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Javascript Code.
+// Javascript Code.f
 $(document).ready(function () {
   // $.get("_posts/2014-11-09.json", function(data){
   //
@@ -9,7 +9,72 @@ $(document).ready(function () {
 
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
-    $(".posts").html(posts['posts'][0]['title']);
-    //
+    order_date(posts["posts"]);
+
+    function order_date(myArray) {
+      var size = myArray.length;
+      for(var pass = 1; pass < size; pass++){ // outer loop
+        for( var up = 0; up < (size - pass); up++){ // inner loop
+          var down = up + 1;
+          if(myArray[up]['date'] < myArray[down]['date']){
+            swap(myArray, down, up);
+          }
+        }
+      }
+    }
+
+    function swap(myArray, indexOne, indexTwo) {
+      var tmpVal = myArray[indexOne];
+      myArray[indexOne] = myArray[indexTwo];
+      myArray[indexTwo] = tmpVal;
+    }
+
+    $("#titulo_1").html(posts['posts'][0]['title']);
+    $(".parrafo_1").html(posts['posts'][0]['content']);
+    $(".fecha_1").html(posts['posts'][0]['date']);
+
+    $("#titulo_2").html(posts['posts'][1]['title']);
+    $(".parrafo_2").html(posts['posts'][1]['content']);
+    $(".fecha_2").html(posts['posts'][1]['date']);
+
+    $("#titulo_3").html(posts['posts'][2]['title']);
+    $(".parrafo_3").html(posts['posts'][2]['content']);
+    $(".fecha_3").html(posts)['posts'][2]['date'];
   });
+
+  $(".search_box").hide();
+  $(".drop_down_1").hide();
+  $(".drop_down_2").hide();
+  $(".drop_down_3").hide();
+
+  $("#titulo_1").click(function() {
+    $(".drop_down_1").addClass("current");
+    $(".current").toggle();
+    $(".drop_down_1").removeClass("current");
+  });
+
+  $("#titulo_2").click(function() {
+    $(".drop_down_2").addClass("current");
+    $(".current").toggle();
+    $(".drop_down_2").removeClass("current");
+  });
+
+  $("#titulo_3").click(function () {
+    $(".drop_down_3").addClass("current");
+    $(".current").toggle();
+    $(".drop_down_3").removeClass("current");
+  });
+
+  $("#comment_button_1").click(function() {
+    var comment = $("input[name=my_comment_1]").val();
+    $("#comments_area_1").append("<p>" + comment + "</p>");
+    $("input[name=my_comment_1]").val("");
+  });
+
+  $("#comment_button_2").click(function() {
+    var comment = $("input[name=my_comment_2]").val();
+    $("#comments_area_2").append("<p>" + comment + "</p>");
+    $("input[name=my_comment_2]").val("");
+  });
+
 });
